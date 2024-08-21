ea0ce973a7cd3c18c7c4b84ab646bd6b8ec7be95
bruce845 <bruce845@hotmail.com>
2024-08-21 02:15:33 -0300

    Update hamachi.sh

--- src/hamachi.sh
+++ src/hamachi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 dpkg -i logmein-hamachi_2.1.0.203-1_amd64.deb
 /etc/init.d/logmein-hamachi start
-nohup /opt/logmein-hamachi/bin/hamachi
+nohup /opt/logmein-hamachi/bin/hamachid
 hamachi login
 hamachi set-nick brucon01
 hamachi join vilaclara_bruce bruce_vilaclara
