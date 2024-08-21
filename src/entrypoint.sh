12a7ee41d0bf5ebfbcee5891aa0aea06ec238b70
bruce845 <bruce845@hotmail.com>
2024-08-21 02:44:38 -0300

    Update entrypoint.sh

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 whoami
 /etc/init.d/logmein-hamachi start
-sleep 10
-ps aux
-echo "Executando Hamachi Login"
-hamachi login
-echo "Setando Nick Hamachi"
-hamachi set-nick brucon01
-echo "Entrando na rede Hamachi"
-hamachi join vilaclara_bruce bruce_vilaclara
 mkdir -vp /run/sshd
 /usr/sbin/sshd -D
