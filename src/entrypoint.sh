86e5ddd8c4754ed90e9b56a5ee170dfed41472c7
bruce845 <bruce845@hotmail.com>
2024-08-21 02:55:15 -0300

    Update entrypoint.sh

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 whoami
-/etc/init.d/logmein-hamachi start
+nohup /etc/init.d/logmein-hamachi start
+ping -c 4 hamachi-data-center.logmein-gateway.com
+sleep 10
+ps aux
 mkdir -vp /run/sshd
 /usr/sbin/sshd -D
