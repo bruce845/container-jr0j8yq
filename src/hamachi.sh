4911c0c740700bb628c70ecff937e995f8a2bdb7
bruce845 <bruce845@hotmail.com>
2024-08-21 02:22:32 -0300

    Update hamachi.sh

--- src/hamachi.sh
+++ src/hamachi.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 dpkg -i logmein-hamachi_2.1.0.203-1_amd64.deb
+ping -c 4 hamachi-data-center.logmein-gateway.com
 /etc/init.d/logmein-hamachi start
 nohup /opt/logmein-hamachi/bin/hamachid
 hamachi login
